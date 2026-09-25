3d0a4de7ff973c8453b01d2e2edbdcf274b43310
Nerea <nerea.garcia.vergara.al@iespoligonosur.org>
2026-09-25 02:57:06 +0200

    Ej02 resuelto

---
+++ ud1/src/main/java/com/ada/flujo_caracteres/ej02.java
@@ -0,0 +1,13 @@
+package com.ada.flujo_caracteres;
+import java.io.*;
+public class ej02 {
+    public static void main(String[] args) throws IOException{
+        FileWriter fw = new FileWriter("ud1/src/main/java/com/ada/flujo_caracteres/texto.txt", true); //PARA SOBRE ESCRIBIR PONEMOS TRUE Y ASI SE GUARDA TODO
+        PrintWriter pw = new PrintWriter(fw);
+        pw.print("Hola Caracola");
+        pw.flush();
+        pw.close();
+        
+    }
+    
+}
